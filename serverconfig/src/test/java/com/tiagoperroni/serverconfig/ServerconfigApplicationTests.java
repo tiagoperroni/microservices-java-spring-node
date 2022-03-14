7b0792f5a29d130867a4a205eefaebd47cb58ef8
Tiago <tiagoperroni86@gmail.com>
2022-03-14 07:40:16 -0300

    sending config properties

---
+++ serverconfig/src/test/java/com/tiagoperroni/serverconfig/ServerconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagoperroni.serverconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
