9cab1204b277c5cd7550d64978fb781ff51e51a5
Tiago <tiagoperroni86@gmail.com>
2022-03-19 11:12:14 -0300

    Add retry config and fallbackmethod adress micros

--- address/src/main/java/com/tiagoperroni/address/controller/AdressController.java
+++ address/src/main/java/com/tiagoperroni/address/controller/AdressController.java
@@ -1,5 +1,8 @@
 package com.tiagoperroni.address.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.tiagoperroni.address.model.Adress;
 
 import org.slf4j.Logger;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.retry.annotation.Retry;
+
 @RestController
 @RequestMapping("/adress")
 public class AdressController {
@@ -23,6 +28,7 @@ public class AdressController {
     private Logger logger = LoggerFactory.getLogger(AdressController.class);
 
     @GetMapping("/{cep}")
+    @Retry(name = "requestAdress", fallbackMethod = "requestAdressFallback")
     public ResponseEntity<Adress> getAdress(@PathVariable("cep") String cep) {
         logger.info("Request received cep: {}", cep);
         ResponseEntity<Adress> adress = restTemplate
@@ -30,6 +36,13 @@ public class AdressController {
         logger.info("Sending adress: {}", adress);
         return new ResponseEntity<>(adress.getBody(), HttpStatus.OK);
         
+    }
+
+    public ResponseEntity<Map<String, String>> requestAdressFallback(Throwable ex) {
+        var error = new HashMap<String, String>();
+        error.put("Error", "Fail when try to send a request to CEP API");
+        error.put("OriginalMessage", ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.SERVICE_UNAVAILABLE);
     }
 }
 
